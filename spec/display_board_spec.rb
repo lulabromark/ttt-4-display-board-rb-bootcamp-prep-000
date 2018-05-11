3e2d0d6f3b01c905c686626be38f383224cc8856
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-11 23:18:22 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -183,7 +183,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
-      expect(rows[0]).to eq(" O | 0 | 0 ")
+      expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
